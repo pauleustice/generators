41b409fd8f1ea4c029e930b0418ab08af9c65565
Paul Eustice <paul.eustice@landg.com>
2020-04-07 15:25:52 +0100

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IntroComponent } from './components/intro/intro.component';
+import { Example1Component } from './components/examples/example1/example1.component';
+import { Example2Component } from './components/examples/example2/example2.component';
+import { Example3Component } from './components/examples/example3/example3.component';
+import { Example4Component } from './components/examples/example4/example4.component';
+import { Example5Component } from './components/examples/example5/example5.component';
+
+
+const routes: Routes = [
+  { path: '', component: IntroComponent },
+  { path: 'examples/1', component: Example1Component },
+  { path: 'examples/2', component: Example2Component },
+  { path: 'examples/3', component: Example3Component },
+  { path: 'examples/4', component: Example4Component },
+  { path: 'examples/5', component: Example5Component },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { Example1Component } from './components/examples/example1/example1.component';
+import { ConsoleComponent } from './components/console/console.component';
+import { Example2Component } from './components/examples/example2/example2.component';
+import { Example3Component } from './components/examples/example3/example3.component';
+import { Example4Component } from './components/examples/example4/example4.component';
+import { Example5Component } from './components/examples/example5/example5.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IntroComponent,
+    Example1Component,
+    ConsoleComponent,
+    Example2Component,
+    Example3Component,
+    Example4Component,
+    Example5Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
