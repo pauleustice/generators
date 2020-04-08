2b7e11d1e6b13914d6eb347fb10f315a88a638cd
Paul Eustice <paul.eustice@landg.com>
2020-04-08 15:45:26 +0100

    Use generator for step counting

--- src/app/components/examples/example5/example5.component.ts
+++ src/app/components/examples/example5/example5.component.ts
@@ -7,14 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Example5Component implements OnInit {
   step = 0;
+  stepCount = 4;
+  stepper = this.stepGenerator(this.stepCount);
 
   constructor() { }
 
   ngOnInit(): void {
+    this.stepper.next();
   }
 
-  updateStep(event) {
-    this.step = Number(event.target.value);
+  *stepGenerator(stepCount) {
+    let i = 0;
+
+    while (i < stepCount + 1) {
+      const input = yield i;
+
+      switch (input) {
+        case -1: {
+          if (i > 0) {
+            i--;
+          }
+          break;
+        }
+        case 1: {
+          if (i < stepCount) {
+            i++;
+          }
+          break;
+        }
+      }
+    }
+  }
+
+  updateStep(direction: number) {
+    const { value: step = 0 } = this.stepper.next(direction);
+    this.step = step;
   }
 
 }
