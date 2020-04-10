973bc24d5ce882df16f9a8b40debd08c56558fd8
Paul Eustice <paul.eustice@landg.com>
2020-04-10 16:30:39 +0100

    Switch order of examples 3 and 4

--- src/app/components/examples/example3/example3.component.ts
+++ src/app/components/examples/example3/example3.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { StepService } from '../../../services/step-service.service';
 
 @Component({
   selector: 'app-example3',
@@ -7,31 +6,10 @@ import { StepService } from '../../../services/step-service.service';
   styleUrls: ['./example3.component.scss']
 })
 export class Example3Component implements OnInit {
-  i = 0;
-  step = 0;
-  stepCount = 19;
-  stepper = this.stepService.stepGenerator(this.stepCount);
 
-  constructor(
-    private stepService: StepService
-  ) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.stepper.next();
   }
 
-  updateStep(direction: number) {
-    const { value: step = 0 } = this.stepper.next(direction);
-    this.step = step;
-
-    if (this.step < 8) {
-      this.i = 0;
-    } else if (this.step < 13) {
-      this.i = 1;
-    } else if (this.step < 18) {
-      this.i = 2;
-    } else {
-      this.i = 3;
-    }
-  }
 }
--- src/app/components/examples/example4/example4.component.ts
+++ src/app/components/examples/example4/example4.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StepService } from '../../../services/step-service.service';
 
 @Component({
   selector: 'app-example4',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./example4.component.scss']
 })
 export class Example4Component implements OnInit {
+  i = 0;
+  step = 0;
+  stepCount = 19;
+  stepper = this.stepService.stepGenerator(this.stepCount);
 
-  constructor() { }
+  constructor(
+    private stepService: StepService
+  ) { }
 
   ngOnInit(): void {
+    this.stepper.next();
   }
 
+  updateStep(direction: number) {
+    const { value: step = 0 } = this.stepper.next(direction);
+    this.step = step;
+
+    if (this.step < 8) {
+      this.i = 0;
+    } else if (this.step < 13) {
+      this.i = 1;
+    } else if (this.step < 18) {
+      this.i = 2;
+    } else {
+      this.i = 3;
+    }
+  }
 }
